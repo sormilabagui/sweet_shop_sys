9336d7797251e56999c130e2b505d0f274f1ceee
sormilabagui <sumontabagui189@gmail.com>
2025-07-16 22:12:30 +0530

    test: added failing test case for delete_sweet() feature

--- sweet_shop.py
+++ sweet_shop.py
@@ -2,11 +2,13 @@ class SweetShop:
     def __init__(self):
         self.sweets = []  # This will store our sweet dictionaries
 
-    def add_sweet(self, name, category, price):
+    def add_sweet(self, sweet_id, name, category, price, quantity):
         sweet = {
+            'id' : sweet_id,
             'name': name,
             'category': category,
-            'price': price
+            'price': price,
+            'qty' : quantity
         }
         self.sweets.append(sweet)
         print("current sweets list:", self.sweets)
--- test_sweet.py
+++ test_sweet.py
@@ -3,11 +3,11 @@ from sweet_shop import SweetShop
 
 def test_add_sweet():
     shop = SweetShop()
-    shop.add_sweet("Rasgulla", "Milk-based", 20.0)
+    shop.add_sweet(1,"Rasgulla", "Milk-based", 20.0,20)
+    shop.add_sweet(2,"Laddo", "Gram-based", 15.0,15)
     
     sweets = shop.get_all_sweets()
     
     assert len(sweets) == 1
-    assert sweets[0]['name'] == "Rasgulla"
-    assert sweets[0]['category'] == "Milk-based"
-    assert sweets[0]['price'] == 20.0
+    assert sweets[0]['name'] == "Ladoo"
+    
